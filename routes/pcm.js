51bd5aa897a10dfbc1e4f5a720d0d1ce7ab8f144
Leandro <leandroklink13@gmail.com>
2026-07-19 16:30:46 -0400

    Continuação das rotas

--- routes/pcm.js
+++ routes/pcm.js
@@ -4,9 +4,17 @@ const express = require('express')
 const router = express.Router() 
 
 
-router.get('/', (req, res) => {
-    console.log('teste')
+router.get('/pcm', (req, res) => {
+    console.log('pcm/dashboard')
 })
+
+router.get('/pcm/solicitacoes', (req, res) => {
+    console.log('pcm/solicitacoes')
+})
+router.get('/pcm/os/:id', (req, res) => {
+    console.log('pcm/solicitacoes')
+})
+
 
 //404
 router.use((req, res) => {
@@ -14,3 +22,25 @@ router.use((req, res) => {
 })
 
 module.exports = router
+
+
+
+/*
+GET  /pcm
+
+GET  /pcm/dashboard
+
+GET  /pcm/solicitacoes
+
+GET  /pcm/os 
+
+GET  /pcm/os/:id
+
+POST /pcm/os/:id/aprovar
+
+POST /pcm/os/:id/rejeitar
+
+POST /pcm/os/:id/finalizar
+
+
+*/
